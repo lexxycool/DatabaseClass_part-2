7f2c301161977863b0857abe601404baa711d4b6
lexxycool <maxirexy16@yahoo.com>
2021-10-05 10:40:48 -0400

    Add files via upload

---
+++ exercise-student/postgres/Exercises/18_city_count.sql
@@ -0,0 +1,10 @@
+-- 18. The count of the number of cities (name column 'num_cities') and the state abbreviation for each state and territory (exclude state abbreviation DC).
+-- Order the results by state abbreviation.
+-- (55 rows)
+
+select count(state_abbreviation)
+as num_cities, state_abbreviation
+from city
+where state_abbreviation != 'DC'
+group by state_abbreviation
+order by state_abbreviation asc;
---
+++ exercise-student/postgres/Exercises/20_largest_city_per_state.sql
@@ -0,0 +1,8 @@
+-- 20. The state abbreviation, and population of the largest city (name column 'city_population') of all states, territories, and districts.
+-- Order the results from highest to lowest populations.
+
+select state_abbreviation, max(population)
+as city_population 
+from city
+group by state_abbreviation
+order by city_population desc;
